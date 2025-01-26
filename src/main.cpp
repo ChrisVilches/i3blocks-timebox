dae95b54c665deeabe2791aeaff05e081081290b
Chris Vilches <felovilches@gmail.com>
2025-01-26 19:41:55 +0900

    refactor main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,28 +14,35 @@ const int step_seconds_big = 60 * 5;
 static std::mutex print_mtx;
 
 void print_line(const std::string& s) {
+  // TODO: Get the message as an object (not string) and
+  // also print it with colors and a clock icon.
   std::lock_guard<std::mutex> lock(print_mtx);
   std::cout << s << std::endl;
 }
 
+// TODO: Weird: it's creating a new timer even though the one I had ended.
+// I don't know if this problem is caused by i3 or by this C++ program.
+// hard to debug lol.
+
+void execute_notification(const int argc, char* argv[]) {
+  if (argc < 3) return;
+  if (fork() != 0) return;
+
+  // Having the program output here will ruin the output of the i3 block!
+  // So it must be silenced.
+  utils::redirect_to_dev_null();
+
+  if (execvp(argv[2], argv + 2) == -1) {
+    // TODO: Change error message for a human-friendly one, although I never read it
+    // really (because it never happens).
+    perror("execvp failed");
+  }
+}
+
 int main(const int argc, char* argv[]) {
-  const std::function<void()> end_cb = [argc, argv]() {
-    if (argc < 3) return;
-    if (fork() != 0) return;
-
-    // Having the program output here will ruin the output of the i3 block!
-    // So it must be silenced.
-    utils::redirect_to_dev_null();
-
-    if (execvp(argv[2], argv + 2) == -1) {
-      perror("execvp failed");
-    }
-  };
-
   // TODO: The only thing I don't know is how to join the thread at the end safely.
   // add a destructor to join or detach the thread, or something similar.
-  Timer timer(end_cb, print_line);
-  timer.start();
+  Timer timer([argc, argv]() { execute_notification(argc, argv); }, print_line);
 
   try {
     const int init_seconds = std::stoi(argv[1]);
