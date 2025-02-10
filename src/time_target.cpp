53db14ffd3b7801d4de01158ba39fb5a65ed6f07
Chris Vilches <felovilches@gmail.com>
2025-02-10 22:11:25 +0900

    remove resolved TODO comment

--- src/time_target.cpp
+++ src/time_target.cpp
@@ -6,13 +6,6 @@ int TimeTarget::ms_until() {
   return std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
 }
 
-// TODO: I'm not sure if the timer runs for X seconds or for X-1 seconds.
-// See? discrete math is the most important math branch for programming.
-// NOTE: Fixed. I changed it to milliseconds_until so that it has more precision
-// (the display is still formatted to just seconds though), but this means that
-// when the number of seconds is between 0 and 1, the value will not be 0 (bc it includes
-// all the ms in between) so this won't run off when the value is for example 999ms (which
-// was being rounded to 0 in the previous implementation)
 bool TimeTarget::is_over() { return ms_until() <= 0; }
 
 void TimeTarget::inc(const int s) {
